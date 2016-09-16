abbd0899d0ee054aba2d1b8a313db5bbf0f2fc73
DaffPunks <sawavas2@gmail.com>
2016-09-17 03:03:02 +0700

    (+) added command line support; implemented AAA protocol; fixed some stuff

--- src/com/god/damn/Main.java
+++ src/com/god/damn/Main.java
@@ -1,8 +1,23 @@
 package com.god.damn;
 
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        new Cli(args).parse();
+        //Test users data
+        ArrayList<User> UserList = new ArrayList<User>();
+        UserList.add(new User(1, "John Doe", "jdoe", "sup3rpaZZ"));
+        UserList.add(new User(2, "Jane Row", "jrow", "Qweqrty12"));
+
+        //Test roles data
+        ArrayList<Role> RoleList = new ArrayList<Role>();
+        RoleList.add(new Role(1, 1, Role.READ, "a"));
+        RoleList.add(new Role(2, 1, Role.WRITE, "a.b"));
+        RoleList.add(new Role(3, 2, Role.EXECUTE, "a.b.c"));
+        RoleList.add(new Role(4, 1, Role.EXECUTE, "a.bc"));
+
+        new Cli(args, UserList, RoleList).parse();
     }
 }
--- src/com/god/damn/Role.java
+++ src/com/god/damn/Role.java
@@ -1,18 +1,20 @@
 package com.god.damn;
 
-/**
- * Created by 1 on 16.09.2016.
- */
-public class Role {//role, aga
-    public int Id;
-    public int User_Id;
-    public String Name;
-    public String Resource; //хз, там не стринг, наверное, но пока пусть так
 
-    public Role(int id, int user_Id, String name, String resource) {
-        Id = id;
-        User_Id = user_Id;
-        Name = name;
+public class Role {
+    final static int READ       = 1;
+    final static int WRITE      = 2;
+    final static int EXECUTE    = 4;
+
+    public int      Id;
+    public int      User_id;
+    public int      Name;
+    public String   Resource;
+
+    public Role(int id, int user_id, int name, String resource) {
+        Id       = id;
+        User_id  = user_id;
+        Name     = name;
         Resource = resource;
     }
 }
