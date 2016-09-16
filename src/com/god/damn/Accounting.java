d2c3243c758347467e7a8a44dcb65c76e71725ff
Vlad <lemon444eg@gmail.com>
2016-09-16 21:57:36 +0700

    (+) добавил в классы переменные, запилил хэш+соль в классе User

--- src/com/god/damn/Accounting.java
+++ src/com/god/damn/Accounting.java
@@ -4,4 +4,17 @@ package com.god.damn;
  * Created by 1 on 16.09.2016.
  */
 public class Accounting {//accounting, aga
+        public int Id;
+        public int Role_Id;
+        public String Amount;
+        public String Date_st;
+        public String Date_end;
+
+        public Accounting(int id, int role_Id, String amount, String date_st, String date_end) {
+                Id = id;
+                Role_Id = role_Id;
+                Amount = amount;
+                Date_st = date_st;
+                Date_end = date_end;
+        }
 }
--- src/com/god/damn/User.java
+++ src/com/god/damn/User.java
@@ -1,8 +1,39 @@
 package com.god.damn;
-
+import java.security.SecureRandom;
+import java.math.BigInteger;
 /**
  * Created by 1 on 16.09.2016.
  */
 public class User {
-    //user tipa, aga
+    public int Id;
+    public String Login;
+    public String Pass;
+    public String Name;
+
+    public User( int id, String login, String pass,String name) {
+
+        Id = id;
+        Login = login;
+        Name = name;
+        // Pass = pass;
+        SecureRandom random = new SecureRandom();
+        String Salt = new BigInteger(130, random).toString(32);
+        Pass=MD5(MD5(pass)+Salt);
+    }
+    private String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
+    }
+
+
+
 }
