cc51cbc06fa0613708c251e5c9807dcd200cd38c
Vlad <lemon444eg@gmail.com>
2016-09-16 19:10:14 +0700

    (+) создал 3 класса

---
+++ src/com/god/damn/Role.java
@@ -0,0 +1,7 @@
+package com.god.damn;
+
+/**
+ * Created by 1 on 16.09.2016.
+ */
+public class Role {//role, aga
+}
---
+++ src/com/god/damn/User.java
@@ -0,0 +1,8 @@
+package com.god.damn;
+
+/**
+ * Created by 1 on 16.09.2016.
+ */
+public class User {
+    //user tipa, aga
+}
