525c1ca41967340879e05a3c667891931bbab053
DaffPunks <sawavas2@gmail.com>
2016-09-16 22:20:29 +0700

    (+) added command lines args

--- src/com/god/damn/Cli.java
+++ src/com/god/damn/Cli.java
@@ -17,27 +17,74 @@ public class Cli {
         this.args = args;
 
         options.addOption("h", "help", false, "show help.");
-        options.addOption("l", "login", true, "your login.");
-        options.addOption("p", "password", true, "your password.");
+        options.addOption("login", "login", true, "your login.");
+        options.addOption("pass", "password", true, "your password.");
+        options.addOption("res", "resource", true, "Requested resource.");
+        options.addOption("role", "role", true, "What you what to do with this resource.");
+        options.addOption("ds", "datestart", true, "Using start date.");
+        options.addOption("de", "dateend", true, "Using end date.");
+        options.addOption("val", "value", true, "Value");
 
     }
 
     public void parse() {
+        int allowInput = 0;
         CommandLineParser parser = new BasicParser();
 
         CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
 
-            if (cmd.hasOption("h"))
+            if (cmd.hasOption("h")){
                 help();
+            }
 
-            if (cmd.hasOption("l")) {
-                System.out.println("Login is: " + cmd.getOptionValue("l"));
+            if (cmd.hasOption("login")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 1;
+                }
+            }
+            if (cmd.hasOption("pass")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 2;
+                }
 
             }
-            if (cmd.hasOption("p")) {
-                System.out.println("Pass is: " + cmd.getOptionValue("l"));
+            if (cmd.hasOption("res")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 4;
+                }
+
+            }
+            if (cmd.hasOption("role")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 8;
+                }
+
+            }
+            if (cmd.hasOption("ds")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 16;
+                }
+
+            }
+            if (cmd.hasOption("de")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 32;
+                }
+
+            }
+            if (cmd.hasOption("val")) {
+                String value = cmd.getOptionValue("login");
+                if(!value.isEmpty()){
+                    allowInput += 64;
+                }
 
             }
 
@@ -45,6 +92,17 @@ public class Cli {
             log.log(Level.SEVERE, "Failed to parse comand line properties");
             help();
         }
+
+        switch (allowInput){
+            case 3:
+            case 15:
+            case 127:
+                break;
+            default:
+                log.log(Level.SEVERE, "Not enough attributes");
+        }
+        System.out.println(allowInput);
+
     }
 
     private void help() {
