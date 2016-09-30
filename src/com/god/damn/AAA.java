5e1231f7c2cd21d974faf5f662bce4573ea275dd
Vlad <lemon444eg@gmail.com>
2016-10-01 01:58:35 +0700

    (r) refactored from  enumeration of exit codes and AAA  to enum

--- src/com/god/damn/AAA.java
+++ src/com/god/damn/AAA.java
@@ -1,5 +1,6 @@
 package com.god.damn;
 
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -7,21 +8,27 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 
+//import static com.god.damn.AAA.Ax3.AUTHENTICATE;
 import static com.god.damn.Permisions.*;
 import static com.god.damn.Secure.MD5;
+//import static com.god.damn.AAA.ExitCodes.*;
+
+import static jdk.nashorn.tools.Shell.SUCCESS;
 
 public class AAA {
-    private final int AUTHENTICATE = 1;
-    private final int AUTHORIZATION = 2;
-    private final int ACCOUNTING = 3;
+//    private final int AUTHENTICATE = 1;
+//    private final int AUTHORIZATION = 2;
+//    private final int ACCOUNTING = 3;
+private enum Ax3{AUTHENTICATE, AUTHORIZATION, ACCOUNTING};
 
-    private final int SUCCESS = 0;
-    private final int WRONGLOGIN = 1;
-    private final int WRONGPASS = 2;
-    private final int UNKNOWNROLE = 3;
-    private final int FORBIDDEN = 4;
-    private final int INCORRECTACTIVITY = 5;
-    // private final enum{SUCCESS}
+//    private final int SUCCESS = 0;
+//    private final int WRONGLOGIN = 1;
+//    private final int WRONGPASS = 2;
+//    private final int UNKNOWNROLE = 3;
+//    private final int FORBIDDEN = 4;
+//    private final int INCORRECTACTIVITY = 5;
+   private enum ExitCodes{SUCCESS, WRONGLOGIN, WRONGPASS, UNKNOWNROLE, FORBIDDEN, INCORRECTACTIVITY};
+    //if(*err.*==.SUCCESS) {выходит ехит код и сообщение об ошибке}
 
     private ArrayList<User> UserList;
     private ArrayList<Role> RoleList;
@@ -40,17 +47,18 @@ public class AAA {
 
         switch (Parameters.size()) {
             case 2:
-                inputResult = AUTHENTICATE;
+                inputResult = Ax3.AUTHENTICATE.ordinal()+1;
                 break;
             case 4:
-                inputResult = AUTHORIZATION;
+                inputResult = Ax3.AUTHORIZATION.ordinal()+1;
                 break;
             case 7:
-                inputResult = ACCOUNTING;
+                inputResult = Ax3.ACCOUNTING.ordinal()+1;
                 break;
             default: {
                 System.err.println("Wrong Parameters");
-                System.exit(SUCCESS);
+                //final int EC = SUCCESS.ordinal();
+                System.exit(ExitCodes.SUCCESS.ordinal());
             }
         }
 
@@ -64,14 +72,14 @@ public class AAA {
 
         try {
             switch (inputResult) {
-                case AUTHENTICATE:
+                case 1/*Ax3.AUTHENTICATE.ordinal()+1*/:        //а че если сюда тупо числа поставить? или это чему то противоречит?
                     authentication(login, pass);
                     break;
-                case AUTHORIZATION:
+                case 2/*Ax3.AUTHORIZATION.ordinal()+1*/:
                     authentication(login, pass);
                     authorization(res, role, currentUser);
                     break;
-                case ACCOUNTING:
+                case 3/*Ax3.ACCOUNTING.ordinal()+1*/:   //я мб и мудак, что так сделал, но оно не компилилось
                     authentication(login, pass);
                     authorization(res, role, currentUser);
                     accounting(ds, de, val, currentRole);
@@ -103,14 +111,14 @@ public class AAA {
 
         if (!finded) {
             System.err.println("Unknown login");
-            System.exit(WRONGLOGIN);
+            System.exit(ExitCodes.WRONGLOGIN.ordinal());
         }
 
         String checkHash = MD5(MD5(password) + user.Salt);
 
         if (!checkHash.equals(user.Pass)) {
             System.err.println("Wrong password");
-            System.exit(WRONGPASS);
+            System.exit(ExitCodes.WRONGPASS.ordinal());
         }
         this.currentUser = user;
     }
@@ -135,7 +143,7 @@ public class AAA {
         //Role is exist?
         if (!Arrays.asList(AvailableRoles).contains(role)) {
             System.err.println("Unknown role");
-            System.exit(UNKNOWNROLE);
+            System.exit(ExitCodes.UNKNOWNROLE.ordinal());
         }
 
         for (Role roles : RoleList)
@@ -147,7 +155,7 @@ public class AAA {
 
         if (!access) {
             System.err.println("Access denied.");
-            System.exit(FORBIDDEN);
+            System.exit(ExitCodes.FORBIDDEN.ordinal());
         }
     }
 
@@ -172,14 +180,14 @@ public class AAA {
             dateend = format.parse(de);
         } catch (java.text.ParseException e) {
             System.out.println("Incorrect activity (invalid date)");
-            System.exit(INCORRECTACTIVITY);
+            System.exit(ExitCodes.INCORRECTACTIVITY.ordinal());
         }
 
         try {
             value = Integer.parseInt(val);
         } catch (java.lang.NumberFormatException e) {
             System.out.println("Incorrect activity (invalid value)");
-            System.exit(INCORRECTACTIVITY);
+            System.exit(ExitCodes.INCORRECTACTIVITY.ordinal());
         }
 
         currentAccount = new Accounting(role.Id, value, datestart, dateend);
--- src/com/god/damn/Permisions.java
+++ src/com/god/damn/Permisions.java
@@ -1,13 +1,13 @@
 package com.god.damn;
 
 public enum Permisions {
-    READ    (1),
-    WRITE   (2),
-    EXECUTE (4);
+    READ(1),
+    WRITE(2),
+    EXECUTE(4);
 
     private int value;
 
-    Permisions(int value){
+    Permisions(int value) {
         this.value = value;
     }
 
