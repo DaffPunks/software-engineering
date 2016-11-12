ae22d51b6b4a8e22069ce143ade4a77056915074
Vlad <lemon444eg@gmail.com>
2016-11-12 11:34:26 +0700

    (+) it has became a web app

--- src/main/java/com/god/damn/Main.java
+++ src/main/java/com/god/damn/Main.java
@@ -6,7 +6,16 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 
     public static void main(String[] args) {
+        int port = 8080;
+        /*
+        Server server = new Server(port);
 
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        // http://localhost:8080/func
+        context.addServlet(new ServletHolder( new SrvltCalculator( ) ),"/func");
+        */
         H2DataBaseManager dbm = new H2DataBaseManager("jdbc:h2:file:./db/SE", "sa", "");
         new AAA(dbm).execute(new Cli(args).parse());
     }
